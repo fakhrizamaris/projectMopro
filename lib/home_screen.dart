eb05dbbebbf4ac36d5c89aa48d12116df92c11d8
fakhriidjamaris <djamarisfakhri@gmail.com>
2023-11-15 10:34:59 +0700

    menambahkan file aplikasi coffeeshop

---
+++ lib/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        padding: EdgeInsets.only(top: 100, bottom: 40),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage("assets/bg2.jpg"),
+            fit: BoxFit.cover,
+            opacity: 0.6,
+          ),
+        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Coffee Shop",
+                style: GoogleFonts.pacifico(fontSize: 50, color: Colors.white),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Galau? Gelisah? Mending ngopi aja!",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1,
+                    ),
+                  )
+                ],
+              )
+            ]),
+      ),
+    );
+  }
+}
